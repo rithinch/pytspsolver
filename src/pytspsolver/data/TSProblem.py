48aca7958c04143ccd4bc651f99036fa20cd35dd
Rithin Chalumuri <chalumuv@uni.coventry.ac.uk>
2019-03-11 22:02:53 +0000

    Updated TSP to use basic adjacency matrix format

--- src/pytspsolver/data/TSProblem.py
+++ src/pytspsolver/data/TSProblem.py
@@ -1,9 +1,23 @@
 class TSProblem():
   
-  def __init__(self, cities=-1, edges=None):
-    self.text = "Hello World"
+  def __init__(self, cities_mx=None):
 
-  def print_text(self):
-    print(self.text)
+    if cities_mx:
+      self._cities = cities_mx
+    else:
+      self._cities = []
+
+  def print_cities_matrix(self):
+
+    n = len(self._cities)
+    print("     ", end='')
+    for i in range(n):
+        print('{:3}'.format(i), end='')
+    print('\n    '+"----"*n)
+    for i in range(n):
+        print('{:2} | '.format(i), end='')
+        for j in range(n):
+            print('{:3}'.format(self._cities[i][j]), end='')
+        print()
 
 
