e77a4de565cbd48f21b0bc40a9d9b9fad3b4dce0
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2019-03-12 18:00:37 +0000

    Updated initialisors to expose greedy search class

--- src/pytspsolver/__init__.py
+++ src/pytspsolver/__init__.py
@@ -6,5 +6,5 @@ from . import experiments
 __all__ = [ 'entities','solvers','utilities', 'experiments']
 __title__ = 'pytspsolver'
 __version__ = '1.0.0'
-__author__ = 'Rithin Chalumuri'
+__author__ = 'Rithin Chalumuri, Genaro Bedenko'
 __license__ = 'GPL 2.0'
--- src/pytspsolver/solvers/__init__.py
+++ src/pytspsolver/solvers/__init__.py
@@ -1,1 +1,2 @@
 from .ExhaustiveSearch import ExhaustiveSearch
+from .GreedySearch import GreedySearch
