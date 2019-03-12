68775eed88aec57e3873869294f67167aa1bd1a4
Rithin Chalumuri <chalumuv@uni.coventry.ac.uk>
2019-03-12 09:31:36 +0000

    Added visualizer utility

---
+++ src/pytspsolver/utilities/Visualizers.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+class Visualizer():
+
+  def __init__(self, results):
+    self._results = results
+  
+  def plot_time_vs_cost(self, name=''):
+    pass
+  
+  def plot_n_vs_time(self, name=''):
+    pass
+    
