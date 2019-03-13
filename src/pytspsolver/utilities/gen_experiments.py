602f8beaf0deab4f0b23a9f7fc5678c57490b33b
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-03-13 12:05:40 +0000

    Update random problem generation method to add a name

--- src/pytspsolver/utilities/gen_experiments.py
+++ src/pytspsolver/utilities/gen_experiments.py
@@ -1,15 +1,15 @@
 from random import randint
 from pytspsolver.entities import TSProblem
 
-def create_random_problem(n):
+def create_random_problem(name, size):
     
-    dist_matrix = [[0 for _ in range(n)] for _ in range(n)]
+    dist_matrix = [[0 for _ in range(size)] for _ in range(size)]
     
-    for i in range(n):
+    for i in range(size):
         
-        for j in range(i+1,n):
+        for j in range(i+1,size):
             v = randint(1,100)
             dist_matrix[i][j] = v
             dist_matrix[j][i] = v
             
-    return TSProblem(dist_matrix)
+    return TSProblem(name, dist_matrix)
