446bf57c86e0149b0240570d608e05f7812796bf
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-03-13 12:40:59 +0000

    Added unit tests for TSProblem entity

--- src/tests/entities/test_TSProblem.py
+++ src/tests/entities/test_TSProblem.py
@@ -1,11 +1,13 @@
 import pytest
+from pytspsolver.entities import TSProblem
 
 class Test_TSProblem(object):
   
-  def test_one(self):
-        x = "this"
-        assert 'h' in x
-  
-  def test_two(self):
-        x = "this"
-        assert 'h' in x
+  def test_init_shouldSucceedWithDefaultValues(self):
+        
+        name = 'Problem1'
+
+        prob = TSProblem(name)
+
+        assert prob.name == 'Problem1'
+        assert type(prob._cities) == list
