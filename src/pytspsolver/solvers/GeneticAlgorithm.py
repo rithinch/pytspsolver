a79098d2e100e1876ec8d6cd8467dd41aae7e568
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-03-19 09:08:19 +0000

    Genetic Algoritm Base

---
+++ src/pytspsolver/solvers/GeneticAlgorithm.py
@@ -0,0 +1,12 @@
+from .SolverBase import SolverBase
+
+class GeneticAlgorithm(SolverBase):
+
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def run(self, tsp_problem):
+        pass
+
+    
+
