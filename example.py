9ba616bc90b7e77808a574667c9bbb8661e2172b
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-03-19 17:04:58 +0000

    Updated example.py to include symetric and asymeteric

--- example.py
+++ example.py
@@ -10,20 +10,22 @@ import matplotlib.pyplot as plt
 # Create the problems
 #problem1 = create_random_problem("TestProb",3)
 #problem2 = create_random_problem(4) 
-problem3 = create_random_problem("Prob5",5)  
+problem3 = create_random_problem("Prob_Asym_",5, asymeteric=True)  
 
 problem3.visualize(plt)
 
-problems = [create_random_problem("Problem_"+str(i),i) for i in range(3,10)]
-
+symeteric_problems = [create_random_problem("Problem_Sym_"+str(i),i) for i in range(3,10)]
+asymeteric_problems = [create_random_problem("Problem_Asym_"+str(i),i, asymeteric=True) for i in range(3,10)]
 # Initialize Solvers
 exhaustive_search = ExhaustiveSearch(time_limit=50)
 greedy_search = GreedySearch()
 
 # Create Experiment
-experiment = Experiment(problems)
+experiment = Experiment()
 
 #experiment.add_problem(problem1)
+#experiment.add_problems(symeteric_problems)
+experiment.add_problems(asymeteric_problems)
 
 # Add solvers to use in experiment
 experiment.add_solver(exhaustive_search)
@@ -42,7 +44,7 @@ visualizer.plot_n_vs_time(plt, greedy_search._name)
 visualizer.plot_n_vs_time(plt, exhaustive_search._name)
 
 visualizer.plot_n_vs_time_all(plt)
-visualizer.plot_time_vs_cost_all(plt, 'Problem_9')
-visualizer.plot_time_vs_cost(plt, exhaustive_search._name, 'Problem_9')
-visualizer.plot_solver_vs_cost(plt, 'Problem_9')
+visualizer.plot_time_vs_cost_all(plt, 'Problem_Asym_9')
+visualizer.plot_time_vs_cost(plt, exhaustive_search._name, 'Problem_Asym_9')
+visualizer.plot_solver_vs_cost(plt, 'Problem_Asym_9')
 visualizer.plot_problem_vs_cost_all(plt)
