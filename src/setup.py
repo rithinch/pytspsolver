62a5df6836a71c61d1a1db08c4e548ebfa291d32
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-03-19 18:40:47 +0000

    Updated setup.py to automatically install required packages

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import pytspsolver
+
+URL = 'https://github.com/rithinch/pytspsolver'
+REQUIRES_PYTHON = ">=3.6.0"
+REQUIRED_PKGS = ["numpy", "matplotlib", "networkx"]
 
 setup (
         name = pytspsolver.__name__,
         version = pytspsolver.__version__,
-        packages = [ 'pytspsolver','pytspsolver/entities','pytspsolver/solvers', 'pytspsolver/utilities', 'pytspsolver/experiments']
+        python_requires=REQUIRES_PYTHON,
+        packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+        install_requires = REQUIRED_PKGS,
+        license='MIT'
 )
