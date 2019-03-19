5fc9903d2a89d7726ca0a200af1e7d541baca185
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-03-19 09:12:26 +0000

    Updated Random generation method to take in low and high parameters

--- src/pytspsolver/utilities/gen_experiments.py
+++ src/pytspsolver/utilities/gen_experiments.py
@@ -1,14 +1,14 @@
 from random import randint
 from pytspsolver.entities import TSProblem
 
-def create_random_problem(name, size):
+def create_random_problem(name, size, low=1, high=100):
     
     dist_matrix = [[0 for _ in range(size)] for _ in range(size)]
     
     for i in range(size):
         
         for j in range(i+1,size):
-            v = randint(1,100)
+            v = randint(low,high)
             dist_matrix[i][j] = v
             dist_matrix[j][i] = v
             
