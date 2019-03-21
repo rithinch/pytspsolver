d73836f15956b16af9e117f297f4a2327151658c
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-03-21 16:09:05 +0000

    Updatd examples to use both GA selection operators

--- example.py
+++ example.py
@@ -20,9 +20,9 @@ problem3 = create_random_problem("Prob_Sym_40",40)
 # Initialize Solvers
 exhaustive_search = ExhaustiveSearch(time_limit=50)
 greedy_search = GreedySearch()
-genetic_algorithm1 = GeneticAlgorithm(generations=1000, mutation_rate=0.05, population_size=200, elite_size=20)
-genetic_algorithm2 = GeneticAlgorithm(name="GA2", generations=1000, mutation_rate=0.01, population_size=200, elite_size=20)
-
+genetic_algorithm1 = GeneticAlgorithm(generations=1000, mutation_rate=0.05, population_size=200, elite_size=20, selection_operator='tournament')
+genetic_algorithm2 = GeneticAlgorithm(name="GA2", generations=1000, mutation_rate=0.01, population_size=200, elite_size=20, selection_operator='tournament')
+genetic_algorithm3 = GeneticAlgorithm(name="GA Roulette", generations=1000, mutation_rate=0.01, population_size=200, elite_size=20, selection_operator='roulette')
 # Create Experiment
 experiment = Experiment()
 
@@ -35,6 +35,7 @@ experiment.add_problem(problem3)
 experiment.add_solver(greedy_search)
 experiment.add_solver(genetic_algorithm1)
 experiment.add_solver(genetic_algorithm2)
+experiment.add_solver(genetic_algorithm3)
 
 # Run the experiment desired number of times
 experiment.run(epoch=3) 
