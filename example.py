d67d07de1b10eb40e201d54be85c9123924477e9
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2019-03-26 21:41:54 +0000

    Updated example script

--- example.py
+++ example.py
@@ -14,7 +14,11 @@ problem3 = create_random_problem("Prob_Sym_40",40)
 
 #problem3.visualize(plt)
 
-#symeteric_problems = [create_random_problem("Problem_Sym_"+str(i),i) for i in range(10,30)]
+symeteric_problems = [create_random_problem("Problem_Sym_"+str(i),i) for i in range(3,30)]
+symeteric_problems.extend([create_random_problem("Problem_Sym_2_"+str(i),i) for i in range(3,130)])
+symeteric_problems.extend([create_random_problem("Problem_Sym_3_"+str(i),i) for i in range(3,130)])
+symeteric_problems.extend([create_random_problem("Problem_Sym_4_"+str(i),i) for i in range(3,130)])
+symeteric_problems.extend([create_random_problem("Problem_Sym_5_"+str(i),i) for i in range(3,130)])
 #asymeteric_problems = [create_random_problem("Problem_Asym_"+str(i),i, asymeteric=True) for i in range(3,10)]
 
 # Initialize Solvers
@@ -26,19 +30,18 @@ genetic_algorithm3 = GeneticAlgorithm(name="GA Roulette", generations=1000, muta
 # Create Experiment
 experiment = Experiment()
 
-experiment.add_problem(problem3)
-#experiment.add_problems(symeteric_problems)
+experiment.add_problems(symeteric_problems)
 #experiment.add_problems(asymeteric_problems)
 
 # Add solvers to use in experiment
 #experiment.add_solver(exhaustive_search)
 experiment.add_solver(greedy_search)
-experiment.add_solver(genetic_algorithm1)
-experiment.add_solver(genetic_algorithm2)
-experiment.add_solver(genetic_algorithm3)
+# experiment.add_solver(genetic_algorithm1)
+# experiment.add_solver(genetic_algorithm2)
+# experiment.add_solver(genetic_algorithm3)
 
 # Run the experiment desired number of times
-experiment.run(epoch=3) 
+experiment.run(epoch=10) 
 results = experiment.get_results()
 
 # Set up Visualizer with experiment results
@@ -48,10 +51,10 @@ visualizer = Visualizer(results)
 
 #visualizer.plot_n_vs_time(plt, greedy_search._name)
 #visualizer.plot_n_vs_time(plt, exhaustive_search._name)
-visualizer.n_vs_time_all_table()
+# visualizer.n_vs_time_all_table()
 
 visualizer.plot_n_vs_time_all(plt)
-visualizer.plot_time_vs_cost_all(plt, 'Prob_Sym_40')
-visualizer.plot_time_vs_cost(plt, genetic_algorithm2._name, 'Prob_Sym_40')
-visualizer.plot_solver_vs_cost(plt, 'Prob_Sym_40')
-visualizer.plot_problem_vs_cost_all(plt)
+# visualizer.plot_time_vs_cost_all(plt, 'Prob_Sym_40')
+# visualizer.plot_time_vs_cost(plt, genetic_algorithm2._name, 'Prob_Sym_40')
+# visualizer.plot_solver_vs_cost(plt, 'Prob_Sym_40')
+# visualizer.plot_problem_vs_cost_all(plt)
