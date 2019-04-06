9b4f58a22c40a927e7268092bd1972656049a430
Rithin Chalumuri <chalumuv@coventry.ac.uk>
2019-04-06 03:34:27 +0100

    New release specs 0.0.6

--- src/setup.py
+++ src/setup.py
@@ -4,9 +4,19 @@ import pytspsolver
 
 URL = 'https://github.com/rithinch/pytspsolver'
 REQUIRES_PYTHON = ">=3.6.0"
-REQUIRED_PKGS = ["numpy", "matplotlib", "networkx"]
+REQUIRED_PKGS = ["numpy", "matplotlib", "networkx", "tsplib95"]
 AUTHOR = 'Rithin Chalumuri'
 AUTHOR_EMAIL = 'rithinch@gmail.com'
+KEYWORDS = ['tsp', 'optimization-experiments', 'python']
+DESCRIPTION = 'Python package for rapid experimentation on travelling salesman problem'
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Travelling Salesman Problem :: Experimentation Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.6',
+  ]
 
 setup (
         name = pytspsolver.__name__,
@@ -15,5 +25,9 @@ setup (
         packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
         install_requires = REQUIRED_PKGS,
         license='MIT',
-        author=AUTHOR
+        author=AUTHOR,
+        url = URL,
+        keywords = KEYWORDS,
+        classifiers = CLASSIFIERS,
+        description= DESCRIPTION
 )
